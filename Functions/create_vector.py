142b8dc9c25791736426b52870f111be7dbf4dc4
Ricardo Accorsi <ric.accorsi@gmail.com>
2025-07-10 16:54:50 -0300

    Update create_vector.py

--- Functions/create_vector.py
+++ Functions/create_vector.py
@@ -1,10 +1,10 @@
 import os
 import pickle
 
-from langchain_community.document_loaders import PDFPlumberLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-from langchain_ollama import OllamaEmbeddings
+from langchain.embeddings import SentenceTransformerEmbeddings
+from Functions.auxiliary import is_reference_chunk, clean_pdf_text, load_and_clean_pdfs
 
 
 def create_chunks():
@@ -21,21 +21,8 @@ def create_chunks():
     Returns:
     - None (saves processed chunks and vector store to disk).
     """
-
-    # Define the path to the folder containing the PDFs
-    pdf_folder_path = '../Papers/'
-
-    # Load all PDF files in the folder using PDFPlumberLoader
-    loaders = [
-        PDFPlumberLoader(os.path.join(pdf_folder_path, fn))
-        for fn in os.listdir(pdf_folder_path)
-        if fn.lower().endswith(".pdf")  # Filter for PDF files only
-    ]
-
     # Load the documents from the PDF files
-    docs = []
-    for loader in loaders:
-        docs.extend(loader.load())
+    docs = load_and_clean_pdfs(folder_path)
 
     # Initialize a text splitter to divide the documents into chunks
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
@@ -43,20 +30,23 @@ def create_chunks():
     # Split the documents into manageable text chunks
     text_splits = text_splitter.split_documents(docs)
 
+    # Keep only non-reference chunks
+    text_splits = [chunk for chunk in text_splits if not is_reference_chunk(chunk.page_content)]
+
     # Save the split text chunks to a pickle file for later use
-    with open("../Database/text_splits.pkl", "wb") as f:
+    with open("Database/text_splits.pkl", "wb") as f:
         pickle.dump(text_splits, f)
 
     print("text_splits was created with success!")
 
     # Initialize the embedding model to generate vector representations
-    embedding = OllamaEmbeddings(model="mistral")
+    embedding = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
 
     # Create a FAISS index from the text chunks using the embeddings
     index = FAISS.from_documents(text_splits, embedding=embedding)
 
     # Save the FAISS index locally
-    index.save_local("../Database/faiss_index")
+    index.save_local("Database/faiss_index")
 
     print("vector store was created with success!")
 
